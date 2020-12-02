c4fd9c981f648e705e4ba4d4bd012f4325a9f503
xujinjun <939160741@qq.com>
2020-12-02 22:00:50 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello nodejs')
+})
+
+app.options('/upload', cors())
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+  res.send(req.file.filename)
+})
+
+app.get('/preview/:key', cors(), (req, res) => {
+  res.sendFile(`uploads/${req.params.key}`, {
+    root: __dirname,
+    headers:{
+      'Content-Type': 'image/jpeg',
+    },
+  }, (error)=>{
+    if(error){
+      console.log(error)
+    }
+  })
+})
+
+var port = process.env.PORT || 8085
+app.listen(port)
